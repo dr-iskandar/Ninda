9c2f00bf372acbf6769b36f8bd95e931c476f47c
dr-iskandar <drachmat.iskandar@gmail.com>
2019-08-09 13:55:23 +0700

    Animation & Scale
    Adding Animation assets and Scale Function

---
+++ Assets/RotationManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    float rotSpeed = 1000;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        transform.Rotate(0, -rotX, 0, Space.Self);
+    }
+}
